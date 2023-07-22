afdcc5de5aa127ea4a114ce0af64e9510a2e8eeb
Zachary W Sarette <zacharywsarette@gmail.com>
2023-07-22 18:07:17 +0900

    Simple player controls

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
 
-    // Update is called once per frame
+    public GameObject player_sprite;
+
+   private Vector2 lastDirection = Vector2.up; // Default starting direction (facing up)
+
     void Update()
     {
-        
+        // Get horizontal and vertical input values
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        // Calculate movement direction
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+
+        // Normalize the movement vector to handle diagonal movement at the same speed
+        movement = movement.normalized;
+
+        // Rotate the player's body
+        if (movement != Vector2.zero)
+        {
+            lastDirection = movement;
+            float angle = Mathf.Atan2(movement.x, -movement.y) * Mathf.Rad2Deg;
+            player_sprite.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+        // Move the player
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 }
